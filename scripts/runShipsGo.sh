58c0643939c88bc1034ac40f3363556d21296413
Jon Ander <jonander.echeverria@gmail.com>
2026-07-05 12:10:40 +0200

    script update

--- scripts/runShipsGo.sh
+++ scripts/runShipsGo.sh
@@ -3,8 +3,8 @@ set -x # show comands in execution
 
 #which docker
 docker ps -a
-CONTAINER_NAME="ships_go-container"
-IMAGE_NAME="ships_go-image"
+CONTAINER_NAME="ships-go"
+IMAGE_NAME="ships-go"
 
 
 echo "Usuario actual: $(whoami)"
@@ -14,30 +14,59 @@ echo ____________________ BORRAR DOCKER
 docker rm $CONTAINER_NAME
 echo ____________________ BORRAR IMAGEN DOCKER
 docker rmi $IMAGE_NAME:latest
-echo ____________________ CLONAR REPO
+
+set -e # exit on error
+
 cd /home/jonbul/servers
 
 CARPETA="ships-go"
 
 # Comprobar si existe
-if [ -d "$CARPETA" ]; then
-    cd $CARPETA
-    git fetch
-    git pull
-else
-    rm -r -f $CARPETA
-    git clone git@github.com:jonbul/ships-go.git
-    cd $CARPETA
+if [ ! -d "$CARPETA" ]; then
+    mkdir $CARPETA
 fi
-# git checkout cleaning
+
+#Download build file
+cd $CARPETA
+#ask for the latest release from github or snapshot
+echo "Do you want to download the latest release or the latest snapshot?"
+echo "1 release"
+echo "2 snapshot"
+read answer
+
+if [ "$answer" -eq 1 ]; then
+    curl -L --fail https://github.com/jonbul/ships-go/releases/latest/download/ships -o ships
+elif [ "$answer" -eq 2 ]; then
+    curl -L --fail https://github.com/jonbul/ships-go/releases/download/latest-snapshot/ships -o ships
+fi
+chmod +x ships
 
 
-cp -f /home/jonbul/servers/files/.env /home/jonbul/servers/$CARPETA/.env
+# Create Dockerfile
+cat <<EOF > Dockerfile
+FROM alpine:3.19
+
+
+WORKDIR /app
+
+# Copia el binario compilado localmente por el script
+COPY ships .
+
+# Asegura permisos de ejecución para el binario
+RUN chmod +x ships
+
+# Expone el puerto 3000
+EXPOSE 3000 3001
+
+# Ejecuta la aplicación
+CMD ["./ships"]
+EOF
 
 echo ____________________ NUEVO DOCKER
 docker build -t $IMAGE_NAME .
 
-
-docker run -d -p 3000:3000 --name $CONTAINER_NAME -v /home/jonbul/servers/files/ssl:/ssl $IMAGE_NAME
+docker run -d -p 3000:3000 -p 3001:3001 --name $CONTAINER_NAME \
+ -v /home/jonbul/servers/files/ssl:/ssl \
+ -v /home/jonbul/servers/files/.env:/app/.env $IMAGE_NAME
 
 docker ps -a
